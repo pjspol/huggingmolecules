0e28baab3bbcc5e6b9cc77e42fe81aa94e07cdc3
pjspol <86651673+pjspol@users.noreply.github.com>
2022-09-02 23:15:19 -0400

    nn.ModuleDict() x2 and comment out validation_step

--- experiments/src/training/training_lightning_module.py
+++ experiments/src/training/training_lightning_module.py
@@ -3,6 +3,7 @@ from typing import Optional, Callable, Type
 import pytorch_lightning as pl
 import torch
 from torchmetrics import Metric
+from torch import nn
 from torch import Tensor
 
 from experiments.src.training.training_metrics import BatchWeightedLoss
@@ -21,8 +22,8 @@ class TrainingModule(pl.LightningModule):
         self.loss_fn = loss_fn
         self.optimizer = optimizer
 
-        self.weighted_loss = {phase: BatchWeightedLoss() for phase in ['train', 'valid', 'test']}
-        self.metric = {phase: metric_cls() for phase in ['train', 'valid', 'test']}
+        self.weighted_loss = nn.ModuleDict({phase: BatchWeightedLoss() for phase in ['train_set', 'valid_set', 'test_set']})
+        self.metric = nn.ModuleDict({phase: metric_cls() for phase in ['train_set', 'valid_set', 'test_set']})
         self.metric_name = metric_cls.__name__.lower()
 
     def cuda(self, device: Optional[int] = None):
@@ -46,17 +47,17 @@ class TrainingModule(pl.LightningModule):
         return loss
 
     def on_train_epoch_start(self) -> None:
-        self.weighted_loss['train'].reset()
-        self.metric['train'].reset()
+        self.weighted_loss['train_set'].reset()
+        self.metric['train_set'].reset()
 
     def training_step(self, batch: BatchEncodingProtocol, batch_idx: int) -> torch.Tensor:
-        return self._step('train', batch, batch_idx)
+        return self._step('train_set', batch, batch_idx)
 
-    def validation_step(self, batch: BatchEncodingProtocol, batch_idx: int) -> torch.Tensor:
-        return self._step('valid', batch, batch_idx)
+    #def validation_step(self, batch: BatchEncodingProtocol, batch_idx: int) -> torch.Tensor:
+        #return self._step('valid_set', batch, batch_idx)
 
     def test_step(self, batch: BatchEncodingProtocol, batch_idx: int) -> torch.Tensor:
-        return self._step('test', batch, batch_idx)
+        return self._step('test_set', batch, batch_idx)
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
         return self.optimizer
